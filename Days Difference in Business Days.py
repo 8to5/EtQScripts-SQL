4e320bf3493f30e51f7515cc626024723bf4d2cc
Tom Gee <geetc@corning.com>
2023-02-17 14:01:37 -0500

    Create Days Difference in Business Days.py

---
+++ Days Difference in Business Days.py
@@ -0,0 +1,3 @@
+ # Calculates the number of business days between two dates. Used in Phase assignment Due Date formula
+ # Note: the function will need to be modified if the business begins to include weekends in assignments
+ print Rdate.getBusinessDaysDifference(currentDate, DueDate)
